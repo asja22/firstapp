6b1638d244e7a78edf677fd7c932ce04a45cae00
as.ja22 <as_ja22@yahoo.com>
2018-10-27 20:30:39 +0800

    emp controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-    def hello
-      render html: "New Chapter - Learning Rails - First Application"
-    end
+   
 end
