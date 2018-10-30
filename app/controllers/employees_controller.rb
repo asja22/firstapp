0edd367ba4e5a7af02b74ea1597593193816fe61
as.ja22 <as_ja22@yahoo.com>
2018-10-30 20:28:58 +0800

    employees index page and links created

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,10 +2,16 @@ class EmployeesController < ApplicationController
   def home
   end
 
-  def list
+  def index
+    @employee=Employee.all
+  end
+
+  def show
+    @employee=Employee.find(params[:id])
   end
 
   def new
+    @employee=Employee.new
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'employees#home'
-  get '/list' , to: 'employees#list'
+  #get '/list' , to: 'employees#list'
   get '/new' , to: 'employees#new'
-  get '/edit' , to: 'employees#edit'
+  #get '/edit' , to: 'employees#edit'
+  
+  resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
